a722e66cac10460119d6f724f7053f17c7cdd146
Max Fr <fruehaufmaximilian@gmail.com>
2019-10-22 14:32:56 +0800

    Add meanshift tracking

--- assignment5/motion.py
+++ assignment5/motion.py
@@ -4,39 +4,55 @@ from skimage.transform import pyramid_gaussian
 from skimage.filters import sobel_h, sobel_v, gaussian
 from skimage.feature import corner_harris, corner_peaks
 
+
 def listdir(path, list_name):
     for file in os.listdir(path):
-        file_path = os.path.join(path,file)
+        file_path = os.path.join(path, file)
         if os.path.isdir(file_path):
-            listdir(file_path,list_name)
-        elif os.path.splitext(file_path)[1]=='.jpg':
+            listdir(file_path, list_name)
+        elif os.path.splitext(file_path)[1] == '.jpg':
             list_name.append(file_path)
     return list_name
 
-def meanShift(dst, track_window, max_iter=100,stop_thresh=1):
-    """meanShift tracking methdo.
+
+def meanShift(dst, track_window, max_iter=100, stop_thresh=1):
+    """meanShift tracking method.
 
     Args:
-        dst - Backproject image 
+        dst - Backproject image
         track_window - (x,y,w,h).
         max_iter - the maximum iteration.
         stop_thresh - the stop thresh value.
     Returns:
-        track_window -(x,y,w,h).
+        track_window - (x,y,w,h).
     """
     completed_iterations = 0
-    
     while True:
-        c,r,w,h = track_window
-        ### YOUR CODE HERE
-        
-        ### END YOUR CODE
-        track_window = (c_new,r_new,w,h)
-        if (np.linalg.norm(my_mean - my_old_mean) < stop_thresh or completed_iterations == max_iter):
-           
+        old_mean = track_window[:2]
+        x, y, w, h = track_window
+        # YOUR CODE HERE
+        x_min, x_max = x, min(x+w, dst.shape[1])
+        y_min, y_max = y, min(y+h, dst.shape[0])
+        center = np.zeros(shape=(2,))
+        for y in range(y_min, y_max):
+            for x in range(x_min, x_max):
+                # TODO: use gaussian kernel to improve detection results
+                center += np.multiply([x, y], dst[y, x])
+        center /= np.sum(dst[y_min:y_max, x_min:x_max])
+
+        center = np.round(center).astype(int)
+        top_left = np.maximum(center - np.array([w//2, h//2]), 0)
+        # print(dst.shape, top_left, y_min, y_max, x_min,
+        #       x_max, dst[y_min:y_max, x_min:x_max].shape)
+
+        # END YOUR CODE
+        track_window = np.array([*top_left, w, h])
+        if np.linalg.norm(track_window[:2] - old_mean) < stop_thresh or completed_iterations == max_iter:
+            print('.', end='', flush=True)
             return track_window
         completed_iterations += 1
-  
+
+
 def lucas_kanade(img1, img2, keypoints, window_size=9):
     """ Estimate flow vector at each keypoint using Lucas-Kanade method.
 
@@ -56,8 +72,8 @@ def lucas_kanade(img1, img2, keypoints, window_size=9):
         - You may use np.linalg.inv to compute inverse matrix
     """
     assert window_size % 2 == 1, "window_size must be an odd number"
-    
-    flow_vectors = []
+
+    flow_vectors = np.zeros(shape=(len(keypoints), 2))
     w = window_size // 2
 
     # Compute partial derivatives
@@ -66,20 +82,22 @@ def lucas_kanade(img1, img2, keypoints, window_size=9):
 
     # For each [y, x] in keypoints, estimate flow vector [vy, vx]
     # using Lucas-Kanade method and append it to flow_vectors.
-    for y, x in keypoints:
+    for idx, (y, x) in enumerate(keypoints):
         # Keypoints can be loacated between integer pixels (subpixel locations).
         # For simplicity, we round the keypoint coordinates to nearest integer.
         # In order to achieve more accurate results, image brightness at subpixel
         # locations can be computed using bilinear interpolation.
         y, x = int(round(y)), int(round(x))
 
-        ### YOUR CODE HERE
-        
-        ### END YOUR CODE
+        A = np.hstack([np.ravel(Ix[y-w:y+w+1, x-w:x+w+1])[np.newaxis].T,
+                       np.ravel(Iy[y-w:y+w+1, x-w:x+w+1])[np.newaxis].T])
+        b = np.ravel(It[y-w:y+w+1, x-w:x+w+1]) * -1
+        x, _ = np.linalg.lstsq(A, b, rcond=None)[0]
 
-    flow_vectors = np.array(flow_vectors)
+        flow_vectors[idx] = x
 
     return flow_vectors
+
 
 def iterative_lucas_kanade(img1, img2, keypoints,
                            window_size=9,
@@ -108,43 +126,50 @@ def iterative_lucas_kanade(img1, img2, keypoints,
         g = np.zeros(keypoints.shape)
 
     flow_vectors = []
-    w = window_size // 2   
+    w = window_size // 2
     # Compute spatial gradients
     Iy, Ix = np.gradient(img1)
 
     for y, x, gy, gx in np.hstack((keypoints, g)):
-        v = np.zeros(2) # Initialize flow vector as zero vector
-        y1 = int(round(y)); x1 = int(round(x))
-       
-        # TODO: Compute inverse of G at point (x1, y1)
-        ### YOUR CODE HERE
-       
-        ### END YOUR CODE
+        v = np.zeros(2)  # Initialize flow vector as zero vector
+        y1 = int(round(y))
+        x1 = int(round(x))
+
+        Ix_w = Ix[y1-w:y1+w+1, x1-w:x1+w+1]
+        Iy_w = Iy[y1-w:y1+w+1, x1-w:x1+w+1]
+
+        G = np.array([[np.sum(Ix_w ** 2), np.sum(Ix_w * Iy_w)],
+                      [np.sum(Iy_w * Iy_w), np.sum(Iy_w ** 2)]])
+
+        G_inv = np.linalg.inv(G)
 
         # iteratively update flow vector
         for k in range(num_iters):
             vx, vy = v
             # Refined position of the point in the next frame
-            y2 = int(round(y+gy+vy)); x2 = int(round(x+gx+vx))
-            
-            # TODO: Compute bk and vk = inv(G) x bk
-            ### YOUR CODE HERE
-           
-            ### END YOUR CODE
+            y2 = int(round(y+gy+vy))
+            x2 = int(round(x+gx+vx))
 
-            # Update flow vector by vk
-            v += vk
+            I1 = img1[y1-w:y1+w+1, x1-w:x1+w+1]
+            I2 = img2[y2-w:y2+w+1, x2-w:x2+w+1]
+            if I1.shape != I2.shape:
+                v = np.zeros(2)
+                print('Fix this: index out of bounds')
+                break
 
-        vx, vy = v
-        flow_vectors.append([vy, vx])
-        
-    return np.array(flow_vectors)
-        
+            dIk_w = I1 - I2
+            bk = np.array([np.sum(dIk_w * Ix_w),
+                           np.sum(dIk_w * Iy_w)])
+            v += np.matmul(G_inv, bk)
+
+        flow_vectors.append(v)
+
+    return flow_vectors
+
 
 def pyramid_lucas_kanade(img1, img2, keypoints,
                          window_size=9, num_iters=5,
                          level=2, scale=2):
-
     """ Pyramidal Lucas Kanade method
 
     Args:
@@ -169,12 +194,13 @@ def pyramid_lucas_kanade(img1, img2, keypoints,
     g = np.zeros(keypoints.shape)
 
     for L in range(level, -1, -1):
-        ### YOUR CODE HERE
-        
-        ### END YOUR CODE
+        # YOUR CODE HERE
+        None
+        # END YOUR CODE
 
     d = g + d
     return d
+
 
 def compute_error(patch1, patch2):
     """ Compute MSE between patch1 and patch2
@@ -190,17 +216,17 @@ def compute_error(patch1, patch2):
     """
     assert patch1.shape == patch2.shape, 'Differnt patch shapes'
     error = 0
-    ### YOUR CODE HERE
-   
-    ### END YOUR CODE
+    # YOUR CODE HERE
+
+    # END YOUR CODE
     return error
+
 
 def track_features(frames, keypoints,
                    error_thresh=1.5,
                    optflow_fn=pyramid_lucas_kanade,
                    exclude_border=5,
                    **kwargs):
-
     """ Track keypoints over multiple frames
 
     Args:
@@ -219,8 +245,8 @@ def track_features(frames, keypoints,
 
     kp_curr = keypoints
     trajs = [kp_curr]
-    patch_size = 3 # Take 3x3 patches to compute error
-    w = patch_size // 2 # patch_size//2 around a pixel
+    patch_size = 3  # Take 3x3 patches to compute error
+    w = patch_size // 2  # patch_size//2 around a pixel
 
     for i in range(len(frames) - 1):
         I = frames[i]
@@ -234,8 +260,10 @@ def track_features(frames, keypoints,
             # 1. the keypoint falls outside the image J
             # 2. the error between points in I and J is larger than threshold
 
-            yi = int(round(yi)); xi = int(round(xi))
-            yj = int(round(yj)); xj = int(round(xj))
+            yi = int(round(yi))
+            xi = int(round(xi))
+            yj = int(round(yj))
+            xj = int(round(xj))
             # Point falls outside the image
             if yj > J.shape[0]-exclude_border-1 or yj < exclude_border or\
                xj > J.shape[1]-exclude_border-1 or xj < exclude_border:
@@ -270,10 +298,12 @@ def IoU(bbox1, bbox2):
     x1, y1, w1, h1 = bbox1
     x2, y2, w2, h2 = bbox2
     score = 0
-    ### YOUR CODE HERE
-    
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    x_left, y_top = max(x1, x2), max(y1, y2)
+    x_right, y_bottom = min(x1 + w1, x2 + w2), min(y1 + h1, y2 + h2)
 
-    return score
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
 
-
+    isct = (x_right - x_left) * (y_bottom - y_top)
+    return float(isct) / (h1 * w1 + h2 * w2 - isct)
